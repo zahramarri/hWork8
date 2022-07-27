8cf08930f1e8906e8986f7e9a6080b55b81dde0b
zahramarri <zahramarri.dev@gmail.com>
2022-07-27 17:27:46 +0430

    add red asterisks

--- app/src/main/java/com/learning/hwork8/RegistryActivity.kt
+++ app/src/main/java/com/learning/hwork8/RegistryActivity.kt
@@ -1,8 +1,14 @@
 package com.learning.hwork8
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import com.learning.hwork8.databinding.ActivityRegistryBinding
+
 
 class RegistryActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegistryBinding
@@ -10,5 +16,33 @@ class RegistryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRegistryBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setRedAsterisks()
+    }
+
+    private fun setRedAsterisks() {
+        addRedAsterisk(binding.edtFullName)
+        addRedAsterisk(binding.edtIdentificationNumber)
+        addRedAsterisk(binding.edtBirthPlace)
+        addRedAsterisk(binding.edtAddress)
+        addRedAsterisk(binding.edtPostalCode)
+    }
+
+    private fun addRedAsterisk(editText: EditText) {
+        val simple = editText.hint as String
+        val colored = " *"
+        val builder = SpannableStringBuilder()
+
+        builder.append(simple)
+        val start = builder.length
+        builder.append(colored)
+        val end = builder.length
+
+        builder.setSpan(
+            ForegroundColorSpan(Color.RED), start, end,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        editText.hint = builder
     }
 }
